153aaa59afbd15ad11fe554d3494e09f55d6570b
Rosemary Wangui <rosecikumary@gmail.com>
2025-05-23 12:45:16 +0300

    Updated DB connection to use Railway credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQLPORT,
 });
 
 pool.getConnection((err)=>{
