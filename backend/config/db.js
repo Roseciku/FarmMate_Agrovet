427eba484023d2d83a8e19ebe49742725ae58182
Rosemary Wangui <rosecikumary@gmail.com>
2025-05-24 01:27:06 +0300

    Changed railway credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT,
 });
 
